a48d309c9604079c6e7996ccb7eb5a321ad8890d
krystal gates <krystal_sabrina@yahoo.com>
2019-06-10 13:25:24 -0500

    initial HTML and exercise directions

---
+++ script.js
@@ -0,0 +1,4 @@
+// Lightning exercise:
+// Create three `<h3>` tags in an HTML page that each contain 
+// any text you want. Use javascript to listen for a click event 
+// on the header tags and `console.log` their text.
