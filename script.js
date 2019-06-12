fe7a7b15616c53e99a97e2a2e4d341e9e6e5d3ea
krystal gates <krystal_sabrina@yahoo.com>
2019-06-11 19:49:09 -0500

    finished exercise using JS event listeners

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 // Create three `<h3>` tags in an HTML page that each contain 
 // any text you want. Use javascript to listen for a click event 
 // on the header tags and `console.log` their text.
+// const h3Arr = document.querySelectorAll("h3")
+
+
+const buttons = document.querySelectorAll("h3")
+for (const button of buttons){
+    button.addEventListener("click", event => {
+        console.log(button.textContent)
+    })
+}
